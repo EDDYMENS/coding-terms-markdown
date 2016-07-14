e6ed55af33cfcc116f803d37ef0dfb60a0d106f2
eddymens <kwasiamantin@gmail.com>
2016-07-14 10:40:00 +0000

    remove old comments

--- dv_sdk.js
+++ dv_sdk.js
@@ -7,19 +7,6 @@
 /* Initizialize library */
 
 //constants
-// window.devless_token = "";
-// window.devless_key = "localhost";
-// window.devless_domain_name = "localhost";
-
-
-
-// //http, data, chrome, chrome-extension, https, chrome-extension-resource.
-// window.devless_request_protocol = "http";
-
-// //change port number if required
-// window.devless_port = 9000;
-
-
 
 window.devless_instance_url = 
 							window.devless_request_protocol+"://"+window.devless_domain_name+":"+window.devless_port;
